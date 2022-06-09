8619e60c1f6ec9f82f403965f2c1b0d67edca16d
faruk <studentfaruk0308@gmail.com>
2022-06-09 20:59:01 +1000

    After Completion

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while 
+    seconds >= 1
+    puts "#{seconds} SECOND(S)!"
+    seconds -=1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+    while 
+        integer >= 1
+        puts "#{integer} SECOND(S)!"
+        integer -=1
+        sleep(1)
+        end
+        return "HAPPY NEW YEAR!"
+end
+
+
+# x = 1
+# while x < 10
+#   puts "#{x} is less than 10"
+#   x += 1
+# end
